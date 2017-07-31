a7086cd6b1be6e6d68493f67d399d66fbc45716b
lymanjohnson <lyman.johnson@gmail.com>
2017-07-31 16:48:29 -0400

    Put shuffle back into script.js for now

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 const maxFlip = 2; //only two cards at a time
 
 function createDeck(numberOfCards) {      // creates a randomized card deck
@@ -17,7 +41,8 @@ function createDeck(numberOfCards) {      // creates a randomized card deck
   return deck;
 }
 
-doTheyMatch(deck){  //to be called after requisite number of cards are flipped, goes through the deck and finds the flipped cards and compares them. Returns the deck with the cards marked "solved" if appropriate. Otherwise it returns them flipped back over.
+//to be called after requisite number of cards are flipped, goes through the deck and finds the flipped cards and compares them. Returns the deck with the cards marked "solved" if appropriate. Otherwise it returns them flipped back over.
+function doTheyMatch(deck){
 
   let isMatch = true; //assume for the time being it's a match
   let currentSet = []; //this will track indices of the flipped cards
