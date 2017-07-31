01e878c8e0f47ab18db652bcf61aaa800b47a729
lymanjohnson <lyman.johnson@gmail.com>
2017-07-31 16:42:10 -0400

    doTheyMatch function draft complete

--- js/script.js
+++ js/script.js
@@ -18,22 +18,32 @@ function createDeck(numberOfCards) {      // creates a randomized card deck
 }
 
 doTheyMatch(deck){  //to be called after requisite number of cards are flipped, goes through the deck and finds the flipped cards and compares them. Returns the deck with the cards marked "solved" if appropriate. Otherwise it returns them flipped back over.
+
   let isMatch = true; //assume for the time being it's a match
-  let currentSet = [];
+  let currentSet = []; //this will track indices of the flipped cards
+
   for (let i=0;i<deck.length;i++){    //goes through the deck
     if (deck[i].hidden == false && deck[i].solved == false){ //if the current card is active but not solved...
       currentSet.push(i); //it stores the card's index. This should happen as many times as maxFlip.
     }
+  }
 
-    let symbolToMatch = deck[currentSet[0]].symbolID; // stores the first selected card's symbol
+  let symbolToMatch = deck[currentSet[0]].symbolID; // stores the first selected card's symbol
 
-    for (let i=1;i<currentSet.length;i++){  //goes through the flipped cards
-      if (deck[currentSet[i]].symbolID != symbolToMatch) {// if the current card doesn't match the first flipped card
-      isMatch = false;
-      }
+  for (let i=1;i<currentSet.length;i++){  //goes through the flipped cards
+    if (deck[currentSet[i]].symbolID != symbolToMatch) {// if the current card doesn't match the first flipped card...
+    isMatch = false; //then there's no match
     }
   }
 
+  for (let i=1;i<currentSet.length;i++){
+    if (isMatch){     //if they matched, mark them solved
+      deck[currentSet[i]].solved = true;
+    }
+    else {            // otherwise flip them back over
+      deck[currentSet[i]].hidden = true;
+    }
+  }
 
 }
 
