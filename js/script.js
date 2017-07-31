03207f241ded0d9be207240ec1a187ae84a2edee
lymanjohnson <lyman.johnson@gmail.com>
2017-07-31 16:17:40 -0400

    Creating js to track deck and related functions

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function createDeck(numberOfCards) {      // creates a randomized card deck
+  let deck = []
+  // console.log(deck);
+  if (numberOfCards%2 != 0){  //there must be an even number of cards
+    numberOfCards += 1;
+  }
+  // console.log(numberOfCards);
+  for (let i=0;i<(numberOfCards/2); i++){
+    deck.push({"symbolID":i,"hidden":true,"solved":false});
+    deck.push({"symbolID":i,"hidden":true,"solved":false});
+    // console.log(i);
+    // console.log(deck);
+  }
+  shuffle(deck);
+  return deck;
+}
+
+isAMatch(deck){  //to be called after two cards are flipped, goes through the deck and finds the two flipped cards and compares them. Returns the deck with the cards marked "solved" if appropriate. Otherwise it returns them flipped back over.
+  let currentPair = [];
+  for (let i=0;i<deck.length;i++){    //goes through the deck
+    if (deck[i].hidden == false && deck[i].solved == false){ //if the current card is active but not solved...
+      currentPair.push(i); //it stores the card's index. This should happen twice.
+    }
+    firstCard  = deck[currentPair[0]];
+    secondCard = deck[currentPair[1]];
+
+    if (firstCard.symbol == secondCard.symbol){  //if the symbols are the same, mark them solved
+      firstCard.solved  = true;
+      secondCard.solved = true;
+    }
+    else { //Otherwise, flip them back over
+      firstCard.hidden  = false;
+      secondCard.hidden = false;
+    }
+  }
+}
+
+const maxFlip = 2; //only two cards at a time
+
+let gameSize = 20;
+let gameDeck = createDeck(gameSize);
+console.log(gameDeck);
