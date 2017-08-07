afb6d05d23c7996d6cf73606e63f5ce02bab6ae1
lymanjohnson <lyman.johnson@gmail.com>
2017-08-07 03:14:05 -0400

    Final

--- js/script.js
+++ js/script.js
@@ -75,8 +75,7 @@ function buttonPressed() { //will redefine the global game parameters based on p
   newGameSize  = parseInt(document.getElementById("game-size").value);
   newMaxFlip   = parseInt(document.getElementById("hand-size").value);
   newTurnDelay = 2;   //[will require a lot more tweaking to make this dynamic]
-  newLives = 1;
-  // newLives     = parseInt(document.getElementById("live-count").value)*newGameSize/newMaxFlip;
+  newLives     = parseInt(document.getElementById("live-count").value)*newGameSize/newMaxFlip;
     // number of mistakes is weighted by the relative game size and hand size
 
   // gameParameters = createGameParameters(15,2,2,60);
